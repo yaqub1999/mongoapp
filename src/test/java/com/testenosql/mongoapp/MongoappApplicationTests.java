0511e013d898f629d1a635ffa311e29e97b327da
Jorge Yaqub <jorge.yaqub.1106@gmail.com>
2024-07-30 13:27:52 -0300

    primeiro commit

---
+++ src/test/java/com/testenosql/mongoapp/MongoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testenosql.mongoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
